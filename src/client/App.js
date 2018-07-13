a713aee75f5b6bff0e5c325470175d987a3a6835
Sandeep Raveesh <crsandy@gmail.com>
2018-07-13 10:47:38 -0700

    using hashrouter

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, Link, HashRouter as Router } from "react-router-dom";
 import "./app.css";
 
 const Home = () => (
