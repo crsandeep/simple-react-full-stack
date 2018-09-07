dc91a913222ccc60ebee123eb8268042a1805935
Tarandeep Singh <tarandeep.singh@scout24.com>
2018-09-07 13:24:37 +0200

    support both text and stream user tweets

--- src/client/TwitterSearch/TwitterSearch.js
+++ src/client/TwitterSearch/TwitterSearch.js
@@ -13,14 +13,24 @@ export default class App extends Component {
       }));
   }
 
+  getTimeLine(e) {
+    e.preventDefault();
+    fetch(`/api/getTimeLine/${e.target.q.value}`)
+      .then(res => res.json())
+      .then(tweets => this.setState({
+        tweets
+      }));
+  }
+
   render() {
     const { user, tweets } = this.state;
     return (
       <div>
         <h1>{user}</h1>
+        <h2> Search for text in twitter api</h2>
         <form onSubmit={this.getTweets.bind(this)}>
           <div>
-            <label htmlFor="mySearch">Search for user</label>
+            <label htmlFor="mySearch">Search for text</label>
             <input
               type="search"
               id="mySearch"
@@ -35,6 +45,27 @@ export default class App extends Component {
             <span className={styles.valid} />
           </div>
         </form>
+
+        <h2> Search for user tweets time-line in twitter api</h2>
+        <form onSubmit={this.getTimeLine.bind(this)}>
+          <div>
+            <label htmlFor="timeLine">Search for user tweets timeline</label>
+            <input
+              type="search"
+              id="timeLine"
+              name="q"
+              placeholder="User IDs are 4–8 characters in length"
+              required
+              size="30"
+              minLength="4"
+              maxLength="8"
+            />
+            <button>Search</button>
+            <span className={styles.valid} />
+          </div>
+        </form>
+
+
         { tweets && tweets.map(tweet => <p>{tweet}</p>) }
       </div>
     );
--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const os = require('os');
-const { getTweets } = require('./twitter');
+const { getTweets, getTimeLine } = require('./twitter');
 
 const app = express();
 
 app.use(express.static('dist'));
+
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
 app.get('/api/getTweets/:search', (req, res) => {
   if (req.params && req.params.search) {
     getTweets(req.params.search).then(data => res.send(data.statuses.map(status => status.text)));
+  }
+});
+
+app.get('/api/getTimeLine/:user', (req, res) => {
+  if (req.params && req.params.user) {
+    getTimeLine(req.params.user).then(data => res.send(data.map(status => status.text)));
   }
 });
 
