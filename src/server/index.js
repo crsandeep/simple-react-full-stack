87fe46495568b22d6d556699afc366249be1ca5e
Ernest Pascual <eepascual@gmail.com>
2019-01-30 03:45:43 +0800

    Update index.js (#37)
    * Update index.js
    
    Fixed errors on port binding.
    
    * Update src/server/index.js
    
    Co-Authored-By: ernestpascual <eepascual@gmail.com>

--- src/server/index.js
+++ src/server/index.js
@@ -5,4 +5,5 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
