bedbd3547a34f3bf9b91091b08bf6603c4f2cadd
Sandeep Raveesh <crsandy@gmail.com>
2019-01-30 02:41:08 +0800

    Update src/server/index.js
    Co-Authored-By: ernestpascual <eepascual@gmail.com>

--- src/server/index.js
+++ src/server/index.js
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log('Listening on port 8080!'));
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
