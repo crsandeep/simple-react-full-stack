e8689544dbdf1515f9205acf33b9a1f24da1ade7
Greg <gregorio.baldari@gmail.com>
2019-11-23 01:11:43 +0000

    Added tracking from json

--- src/client/App.js
+++ src/client/App.js
@@ -6,22 +6,41 @@ import BodiesCtrPnl from './components/BodiesControllerPanel.js';
 import BodyComponent from './components/BodyComponent.js';
 import InstrCmp from './components/InstrumentComponent.js';
 import MstSoundComponent from './components/MasterSoundComponent.js'
-import Monitor from './components/MonitorComponent.js'
+import Monitor from './components/MonitorComponent.js';
+import data from './lmac1.json';
+
+
+//const json = require('./jse.json');
+
+var index = 0;
+
+function Kinect() {
+  console.log('hello');
+}
 
 
 class App extends Component {
 
+  constructor(props) {
+
+    super(props);
+    this.state = {bodies:[], index:1};
+  }
+
+  componentDidMount() {
+        setInterval(() => {
+            var newIndex = this.state.index + 1;
+            var body = data[newIndex];
+            //console.log(body[0].bodyIndex);
+          //  console.log(body[0].joints);
+            this.setState({bodies:data[newIndex], index: newIndex})
+        }, 0.015);
+    }
+
   render() {
     return (
       <div>
-      <MstSoundComponent />
-      <InstrCtrPnl />
-      <InstrCmp />
-      <BodiesCtrPnl />
-      <div>
-      <Monitor />
-      <BodyComponent />
-      </div>
+      <Monitor bodies={this.state.bodies} />
       </div>
     );
   }
--- src/client/components/MonitorComponent.js
+++ src/client/components/MonitorComponent.js
@@ -6,8 +6,38 @@ import ReactDOM from 'react-dom';
 
 
 class Monitor extends Component {
+
+  constructor(props){
+      super(props)
+  }
+
+  componentDidUpdate() {
+    console.log('Welcome in Monitor Component');
+    if(this.props.bodies !=undefined){
+    console.log('body: ', + this.props.bodies[0].bodyIndex);
+    console.log('all jpoints ', this.props.bodies[0].joints);
+    var joit7 = this.props.bodies[0].joints[7];
+    console.log('x coord: ', joit7.x);
+    this.draw(this.props.bodies[0]);
+   }
+  }
+
+  draw(body){
+    var canvas = document.getElementById('bodyCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#ff0000';
+    var i=0;
+    for (i =0 ; i<21; ++i) {
+      var j= body.joints[i];
+      ctx.fillRect(j.x,j.y, 10, 10);
+
+    }
+
+  }
+
   render() {
-    return <div> Tracking Monitor </div>
+    return <canvas class="border ml-1" ref="canvas" id="bodyCanvas" width="512" height="424"></canvas>
     }
 }
 
