4f80e97f265a4d7ba50322a6716b0327df5596a0
Greg <gregorio.baldari@gmail.com>
2019-12-06 17:02:29 +0000

    dsds

---
+++ src/client/components/InstrumentComponent.js
@@ -0,0 +1,26 @@
+// One for
+// - Only Hands
+// - Spine
+// - Full Body
+// - Only Feet
+//
+// Each one controlls
+// - Type of instrument
+// - Volume
+// - On and Off
+// - Sensitivity
+// - Effetc
+
+//Contains all the instrument
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class InstrCmp extends Component {
+  render() {
+    return <div> Instruments Component </div>
+    }
+}
+
+export default InstrCmp;
