99c863869da08728d4c0b7dbb56b04ab84d4c57c
Metarock <esang037@gmail.com>
2021-03-14 21:13:57 +1300

    Added testing unit and folder

---
+++ test/jest.test.js
@@ -0,0 +1,9 @@
+const user = require('./jest.js');
+
+//toEqual
+test('User should be John Sangalang object', () => {
+    expect(user.createUser()).toEqual({
+        firstName: 'John', 
+        lastName: 'Sangalang'
+    });
+}); 
