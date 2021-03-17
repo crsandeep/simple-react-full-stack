0274280c74c4ce0e9275a11b045e99d1e5c936c7
Metarock <esang037@gmail.com>
2021-03-17 22:55:39 +1300

    please work

--- src/server/index.js
+++ src/server/index.js
@@ -6,4 +6,6 @@ const app = express();
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+const port = process.env.PORT || 8080;
+
+app.listen(port, process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
