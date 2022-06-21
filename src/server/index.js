8312bff97a825a2e52b80196a2b6db95ec7515ce
kmantinaos-ias <107168216+kmantinaos-ias@users.noreply.github.com>
2022-06-21 10:28:22 -0400

    user-agent read and logged from output file

--- src/server/index.js
+++ src/server/index.js
@@ -11,16 +11,23 @@ app.get('/api/sendHeader', (req, res) => {
 
 	// save headers in an object
 	for (i=0; i<headerStrings.length; i+=2){
-		headers[headerStrings[i]] = headerStrings[i+1]
+		headers[headerStrings[i].toLowerCase()] = headerStrings[i+1]
 	}
 
 	// write a JSONified string of the headers to a file
 	const formattedHeaders = JSON.stringify(headers)
 	fs.writeFile('output.json', formattedHeaders, 'utf8', (err) => {
+		
 		if (err) {
 			console.log("An error occured saving the headers")
 			console.error(err)
+
+		// return user-agent from JSON file to verify success
 		} else {
+			rawFileData = fs.readFileSync('output.json')
+			fileData = JSON.parse(rawFileData)
+
+			console.log("User-Agent", fileData["user-agent"])
 			console.log("Headers Saved Succesfully!")
 		}
 	})
