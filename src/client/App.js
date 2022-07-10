9d3b45d01c6ea201f3f12357b24a8ce14f1369e9
erezcohen <erez.cohen@gmail.com>
2022-07-10 23:05:44 +0300

    ip

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,11 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
 import ReactImage from './react.png';
 
 const App = () => {
   const [userName, setUserName] = useState('');
+  const [ip, setIp] = useState('');
+//  const [port, setPort] = useState('');
 
   useEffect(() => {
     fetch('/api/getUsername')
@@ -11,9 +13,29 @@ const App = () => {
       .then(user => setUserName(user.username));
   }, [])
 
+  const onIpChange = (event) =>
+    setIp(event.target.value);
+
   return (
     <div>
-      {userName ? <h1>{`Hello ${userName}`}</h1> : <h1>Loading.. please wait!</h1>}
+      {userName
+        ? <h1>{`Hello ${userName}`}</h1>
+        : <h1>Loading.. please wait!</h1>}
+
+      <div>
+        <input
+          type='text'
+          placeholder='Enter server IP address'
+          onChange={onIpChange}
+          autoFocus
+          value={ip}
+        />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <button>Connect</button>
+      </div>
+
+      <p/>
+
       <img src={ReactImage} alt="react" />
     </div>
   );
