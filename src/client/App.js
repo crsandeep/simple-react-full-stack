18e738028ef0fc955b09bc7f426bd081d67d8904
erezcohen <erez.cohen@gmail.com>
2022-07-10 22:14:38 +0300

    func component

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './app.css';
 import ReactImage from './react.png';
 
-export default class App extends Component {
-  state = { username: null };
+const App = () => {
+  const [userName, setUserName] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('/api/getUsername')
       .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+      .then(user => setUserName(user.username));
+  }, [])
 
-  render() {
-    const { username } = this.state;
-    return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+  return (
+    <div>
+      {userName ? <h1>{`Hello ${userName}`}</h1> : <h1>Loading.. please wait!</h1>}
+      <img src={ReactImage} alt="react" />
+    </div>
+  );
 }
+
+export default App;
--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,11 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.get('/api/getUsername',
+  (req, res) =>
+    res.send({ username: os.userInfo().username }));
+
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Listening on port ${process.env.PORT || 8080}!`));
